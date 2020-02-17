fe2441577144122aa4f587b3f3b0bac94cabbc9d
kgh2927845 <jupiter720@outlook.com>
2020-02-17 09:16:31 +0100

    add migration script

--- contracts/KYC/IKYC.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.16;
-
-interface KYCInterface {
-    event Whitelisted(address who, uint128 nonce);
-
-    function isWhitelisted(address who) external view returns(bool);
-}
---
+++ migrations/2_initial_openbooktoken.js
@@ -0,0 +1,20 @@
+const TOKEN = artifacts.require("./OpenBookToken.sol");
+
+require('openzeppelin-test-helpers/configure')({ web3 });
+const { singletons } = require('openzeppelin-test-helpers');
+
+const CERTIFICATE_SIGNER = '0x91620735349a0B25750facc8e3354c9f02B1518B';
+const controller = '0x91620735349a0B25750facc8e3354c9f02B1518B';
+
+
+module.exports = async function (deployer, network, accounts) {
+
+    if (network === 'development')  {
+        // In a test environment an ERC777 token requires deploying an ERC1820 registry
+        await singletons.ERC1820Registry(accounts[0]);
+    }
+
+    await deployer.deploy(TOKEN, 'OpenBook', 'OBK', 1, [controller], CERTIFICATE_SIGNER);
+    const token = await TOKEN.deployed();
+    console.log("Token address:", token.address);
+};
