2189790699b101f2a719a71939374c0f74ca63a8
kgh2927845 <jupiter720@outlook.com>
2020-02-25 11:07:15 +0100

    update migration script

--- migrations/2_initial_openbooktoken.js
+++ migrations/2_initial_openbooktoken.js
@@ -1,10 +1,16 @@
 const TOKEN = artifacts.require("./OpenBookToken.sol");
+const Dividend = artifacts.require("./Dividend.sol");
+const Ballot = artifacts.require("./Ballot.sol");
 
 require('openzeppelin-test-helpers/configure')({ web3 });
 const { singletons } = require('openzeppelin-test-helpers');
 
 const CERTIFICATE_SIGNER = '0x91620735349a0B25750facc8e3354c9f02B1518B';
 const controller = '0x91620735349a0B25750facc8e3354c9f02B1518B';
+const documentURI = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'; // SHA-256 of documentURI
+const docProposal = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+const documentHash = '0x1c81c608a616183cc4a38c09ecc944eb77eaff465dd87aae0290177f2b70b6f8'; // SHA-256 of documentURI + '0x';
+const ballotOfficialName = 'openBookBallot'
 
 
 module.exports = async function (deployer, network, accounts) {
@@ -18,4 +24,12 @@ module.exports = async function (deployer, network, accounts) {
 
     const token = await TOKEN.deployed();
     console.log("Token address:", token.address);
+
+    await deployer.deploy(Dividend, token.address, CERTIFICATE_SIGNER);
+    const dividend = await Dividend.deployed();
+    console.log("Dividend Contract address:", dividend.address);
+
+    await deployer.deploy(Ballot, ballotOfficialName, docProposal, documentURI, documentHash, token.address);
+    const ballot = await Dividend.deployed();
+    console.log("Ballot Contract address:", ballot.address);
 };
